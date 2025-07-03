8e4979d7644f2a499d3c7d044a0c196cac043e73
CCCPCoder <49943369+CCCPCoder@users.noreply.github.com>
2025-07-03 18:35:03 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,22 @@
+<footer class="footer">
+
+   <section class="grid">
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="Https://wa.me/6281213521529/" target="_blank"><i class="fas fa-phone"></i> +62 81213521529</a>
+         <a href="Https://wa.me/6281212133817/" target="_blank"><i class="fas fa-phone"></i> +62 81212133817</a>
+         <a href="Https://mailto:muhammadnurgian77@gmail.com" target="_blank"><i class="fas fa-envelope"></i> muhammadnurgian77@gmail.com</a>
+         <a href="https://www.google.com/myplace" target="_blank"><i class="fas fa-map-marker-alt"></i> Indonesia, Jakarta</a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://x.com/Komputech_Real" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Kelompok PWL</span> | all rights reserved!</div>
+
+</footer>
